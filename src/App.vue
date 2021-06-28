53235f7c6bdd1d74f30a0e6ba5a712ebf38599c0
Saepudin <saepudinjeh@gmail.com>
2021-06-28 19:32:38 +0700

    adding color scrollbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="2xl:container mx-auto">
+  <div class="2xl:container mx-auto scrollbar-thin scrollbar-thumb-yellow-600 scrollbar-track-yellow-300 h-screen">
     <router-view></router-view>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio')
+    require('@tailwindcss/aspect-ratio'),
+    require('tailwind-scrollbar')
   ],
 }
