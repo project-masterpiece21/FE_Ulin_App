c2fdda2643e184b815e8e17ab4fbccca00ffd210
Saepudin <saepudinjeh@gmail.com>
2021-07-16 18:46:16 +0700

    update CardListComponent

--- src/components/pages/rankComponents/assetComponents/CardListRankComponent.vue
+++ src/components/pages/rankComponents/assetComponents/CardListRankComponent.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="w-full h-screen bg-white md:px-10 py-6 px-5 mt-20 rounded-lg">
 		<div class="flex items-center justify-between pb-2 border-b">
-			<div class="flex sm:flex-row flex-col truncate sm:text-base text-xs">
+			<div class="flex lg:flex-row flex-col truncate sm:text-base text-xs">
 				<span>Peringkat menurut : </span> 
-				<span class="font-bold truncate">pencarian</span>
+				<p class="font-bold truncate"> Pencarian</p>
 			</div>
 
 			<select name="role" class="border-none shadow-none sm:text-sm text-xs bg-gray-50 focus:ring-2 ring-gray-200 focus:ring-gray-300 font-medium w-36">
