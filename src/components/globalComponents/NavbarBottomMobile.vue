f04e5b09bbe1664c1b0a4573444f2cea8ad987fb
Saepudin <saepudinjeh@gmail.com>
2021-07-19 17:02:57 +0700

    update padding navbar bottom mobile

--- src/components/globalComponents/NavbarBottomMobile.vue
+++ src/components/globalComponents/NavbarBottomMobile.vue
@@ -1,7 +1,7 @@
 <template>
 	<nav class="sm:hidden w-full fixed border-t bottom-0 inset-x-0 flex text-xss bg-white z-10">
 
-		<a :href="menu.link" v-for="menu in navMenu" :key="menu.name" :name="menu.name" class="w-full focus:outline-none block py-3 text-center md:mb-8 group cursor-pointer">
+		<a :href="menu.link" v-for="menu in navMenu" :key="menu.name" :name="menu.name" class="w-full focus:outline-none block py-2 text-center group cursor-pointer">
 			<span :class="$route.path === menu.link ? 'text-yellow-500' : 'text-gray-400'" class="iconify text-xl mx-auto  group-hover:text-yellow-500" :data-icon="menu.icon" data-inline="false"></span>
 
 			<div :class="$route.path === menu.link ? 'text-yellow-500' : ''" class="text-gray-500 group-hover:text-yellow-500">
