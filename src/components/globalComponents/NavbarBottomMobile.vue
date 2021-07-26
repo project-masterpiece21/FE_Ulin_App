823d9e02839fe2fa5622bcac01cd80940650236f
Saepudin <saepudinjeh@gmail.com>
2021-07-26 14:12:40 +0700

    change position z-index navbars

--- src/components/globalComponents/NavbarBottomMobile.vue
+++ src/components/globalComponents/NavbarBottomMobile.vue
@@ -1,32 +1,70 @@
 <template>
-	<nav class="sm:hidden w-full fixed border-t bottom-0 inset-x-0 flex text-xss bg-white z-10">
+  <nav
+    class="
+      sm:hidden
+      w-full
+      fixed
+      border-t
+      bottom-0
+      inset-x-0
+      flex
+      text-xss
+      bg-white
+      z-50
+    "
+  >
+    <a
+      :href="menu.link"
+      v-for="menu in navMenu"
+      :key="menu.name"
+      :name="menu.name"
+      class="
+        w-full
+        focus:outline-none
+        block
+        py-2
+        text-center
+        group
+        cursor-pointer
+      "
+    >
+      <span
+        :class="$route.path === menu.link ? 'text-yellow-500' : 'text-gray-400'"
+        class="iconify text-xl mx-auto group-hover:text-yellow-500"
+        :data-icon="menu.icon"
+        data-inline="false"
+      ></span>
 
-		<a :href="menu.link" v-for="menu in navMenu" :key="menu.name" :name="menu.name" class="w-full focus:outline-none block py-2 text-center group cursor-pointer">
-			<span :class="$route.path === menu.link ? 'text-yellow-500' : 'text-gray-400'" class="iconify text-xl mx-auto  group-hover:text-yellow-500" :data-icon="menu.icon" data-inline="false"></span>
-
-			<div :class="$route.path === menu.link ? 'text-yellow-500' : ''" class="text-gray-500 group-hover:text-yellow-500">
-				{{menu.name}}
-			</div>
-		</a>
-		
-	</nav>
+      <div
+        :class="$route.path === menu.link ? 'text-yellow-500' : ''"
+        class="text-gray-500 group-hover:text-yellow-500"
+      >
+        {{ menu.name }}
+      </div>
+    </a>
+  </nav>
 </template>
 
 <script>
-	import { reactive } from "vue";
-	export default {
-		setup() {
-			const navMenu = reactive([
-				{ name: "Cari tempat", icon: "ri:map-pin-fill", link:"/destination"},
-				{ name: "Moment", icon: "ri:ghost-smile-fill", link:"/moment"},
-				{ name: "Beranda", icon: "bx:bxs-home-smile", link:"/"},
-				{ name: "Peringkat", icon: "icomoon-free:trophy", link:"/rank"},
-				{ name: "Pusat info", icon: "ph:newspaper-clipping-fill", link:"/information" },
-			])
+import { reactive } from "vue";
 
-			return {
-				navMenu,
-			}
-		}
-	}
+export default {
+  setup() {
+    const navMenu = reactive([
+      { name: "Cari tempat", icon: "ri:map-pin-fill", link: "/destination" },
+      { name: "Moment", icon: "ri:ghost-smile-fill", link: "/moment" },
+      { name: "Beranda", icon: "bx:bxs-home-smile", link: "/" },
+      { name: "Peringkat", icon: "icomoon-free:trophy", link: "/rank" },
+      {
+        name: "Pusat info",
+        icon: "ph:newspaper-clipping-fill",
+        link: "/information",
+      },
+    ]);
+
+    return {
+      navMenu,
+    };
+  },
+};
 </script>
--- src/components/globalComponents/TopNavbar.vue
+++ src/components/globalComponents/TopNavbar.vue
@@ -1,51 +1,93 @@
 <template>
-	<header class="fixed top-0 2xl:container z-20 w-full md:bg-white bg-gray-100">
-		<slot></slot>
+  <header class="fixed top-0 2xl:container z-50 w-full md:bg-white bg-gray-100">
+    <slot></slot>
 
-		<div class="flex w-full justify-between sm:px-8 px-4 items-center h-20">
+    <div class="flex w-full justify-between sm:px-8 px-4 items-center h-20">
+      <a href="/" class="w-24">
+        <img src="../../assets/img/logo/logo.png" alt="logo-ulin" />
+      </a>
 
-			<a href="/" class="w-24">
-				<img src="../../assets/img/logo/logo.png" alt="logo-ulin">
-			</a>
+      <div class="relative lg:block hidden w-3/5">
+        <form class="relative w-full text-sm pb-5 overflow-hidden">
+          <input
+            type="search"
+            name="search"
+            class="
+              focus:border-2 focus:border-yellow-500
+              px-6
+              focus:outline-none
+              border
+              py-2
+              rounded-xl
+              w-full
+              shadow
+            "
+            placeholder="Cari tempat destinasi wisata, tempat nongkrong, caffe"
+          />
 
-			<div class="relative lg:block hidden w-3/5">
+          <button
+            type="sumbit"
+            class="
+              absolute
+              top-1
+              right-1
+              py-1
+              px-4
+              rounded-lg
+              text-2xl
+              bg-white
+              text-gray-500
+              hover:bg-gray-100
+            "
+          >
+            <span
+              class="iconify"
+              data-icon="bx:bx-search-alt"
+              data-inline="false"
+            ></span>
+          </button>
+        </form>
 
-				<form class="relative w-full text-sm pb-5 overflow-hidden">
-					<input type="search" name="search" class="focus:border-2 focus:border-yellow-500 px-6 focus:outline-none border py-2 rounded-xl w-full shadow" placeholder="Cari tempat destinasi wisata, tempat nongkrong, caffe">
+        <div
+          class="
+            absolute
+            left-3
+            -bottom-0.5
+            text-xs text-gray-500
+            font-medium
+            truncate
+          "
+        >
+          <a href="/" class="mr-4 hover:text-yellow-600 truncate"
+            >desa cibuntu</a
+          >
+          <a href="/" class="mr-4 hover:text-yellow-600 truncate"
+            >taman kanggraksan</a
+          >
+          <a href="/" class="mr-4 hover:text-yellow-600 truncate">penyaweuan</a>
+          <a href="/" class="mr-4 hover:text-yellow-600 truncate"
+            >cadas gantung</a
+          >
+        </div>
+      </div>
 
-					<button type="sumbit" class="absolute top-1 right-1 py-1 px-4 rounded-lg text-2xl bg-white text-gray-500 hover:bg-gray-100">
-						<span class="iconify" data-icon="bx:bx-search-alt" data-inline="false"></span>
-					</button>
-				</form>
+      <div class="flex items-center">
+        <!-- <NavbarIsLogin /> -->
 
-				<div class="absolute left-3 -bottom-0.5 text-xs text-gray-500 font-medium truncate">
-					<a href="/" class="mr-4 hover:text-yellow-600 truncate">desa cibuntu</a>
-					<a href="/" class="mr-4 hover:text-yellow-600 truncate">taman kanggraksan</a>
-					<a href="/" class="mr-4 hover:text-yellow-600 truncate">penyaweuan</a>
-					<a href="/" class="mr-4 hover:text-yellow-600 truncate">cadas gantung</a>
-				</div>
-			</div>
-
-			<div class="flex items-center">
-<!-- 
-				<NavbarIsLogin /> -->
-				
-				<NavbarIsNotLogin />
-			</div>
-			
-		</div>
-	</header>
+        <NavbarIsNotLogin />
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
-	import NavbarIsLogin from './assetComponents/NavigationIsLogin.vue';
-	import NavbarIsNotLogin from './assetComponents/NavigationIsNotLogin.vue';
-	import DropdownMenu from './assetComponents/DropdownMenu.vue';
-	export default {
-		components: {
-			NavbarIsLogin,
-			NavbarIsNotLogin,
-			DropdownMenu
-		}
-	}
+// import NavbarIsLogin from "./assetComponents/NavigationIsLogin.vue";
+import NavbarIsNotLogin from "./assetComponents/NavigationIsNotLogin.vue";
+
+export default {
+  components: {
+    NavbarIsNotLogin,
+    // NavbarIsLogin,
+  },
+};
 </script>
