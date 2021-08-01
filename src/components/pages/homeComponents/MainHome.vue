a975b6fefd2ffb369034f3b089e372d7a923cd3f
Saepudin <saepudinjeh@gmail.com>
2021-08-01 10:43:09 +0700

    change padding homepage mobile

--- src/components/pages/homeComponents/MainHome.vue
+++ src/components/pages/homeComponents/MainHome.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="md:flex xl:ml-52 sm:ml-20 mt-24">
-    <section class="lg:w-3/4 w-full px-4">
+    <section class="lg:w-3/4 w-full px-1">
       <Banner />
       <CategoryDestination />
       <CardList />
