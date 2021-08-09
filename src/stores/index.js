a817f620ea45f0fc50de0622e84e0668f5ae9176
Saepudin <saepudinjeh@gmail.com>
2021-08-09 14:57:47 +0700

    add vuex3

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,11 +1,44 @@
-import { createStore} from 'vuex';
-
-import places from './modules/places';
+import { createStore } from 'vuex';
 
 const stores = createStore({
-  modules: {
-    places
-  }
+  state() {
+    return {
+      places: {},
+      placeById: {},
+    };
+  },
+  getters: {
+    getterPlaces(state) {
+      return state.places;
+    },
+    getterPlaceById(state) {
+      return state.placeById;
+    },
+  },
+  mutations: {
+    setPlaces(state, payload) {
+      state.places = payload;
+    },
+    setPlaceById(state, payload) {
+      state.placeById = payload;
+    },
+  },
+  actions: {
+    async getPlaces({ commit }) {
+      const response = await fetch('https://ulin-api.herokuapp.com/v1/places');
+
+      const { data } = await response.json();
+
+      commit('setPlaces', data);
+    },
+
+    async getPlaceById({ commit }, params) {
+      const response = await fetch(`http://ulin-api.herokuapp.com/v1/place/${params}`);
+
+      const { data } = await response.json();
+      commit('setPlaceById', data);
+    },
+  },
 });
 
 export default stores;
--- src/stores/modules/places.js
+++ src/stores/modules/places.js
@@ -1,42 +1,42 @@
-export default {
-  namespace: true,
-  state: {
-    places: [],
-    placeById: [],
-  },
+// export default {
+//   namespaced: true,
+//   state: {
+//     places: {},
+//     placeById: {},
+//   },
 
-  getters: {
-    getterPlaces(state) {
-      return state.places;
-    },
-    getterPlaceById(state) {
-      return state.placeById;
-    },
-  },
+//   getters: {
+//     getterPlaces(state) {
+//       return state.places;
+//     },
+//     getterPlaceById(state) {
+//       return state.placeById;
+//     },
+//   },
 
-  mutations: {
-    setPlaces(state, payload) {
-      state.places = payload;
-    },
-    setPlaceById(state, payload) {
-      state.placeById = payload;
-    },
-  },
+//   mutations: {
+//     setPlaces(state, payload) {
+//       state.places = payload;
+//     },
+//     setPlaceById(state, payload) {
+//       state.placeById = payload;
+//     },
+//   },
 
-  actions: {
-    async getPlaces({ commit }) {
-      const response = await fetch('https://ulin-api.herokuapp.com/v1/places');
+//   actions: {
+//     async getPlaces({ commit }) {
+//       const response = await fetch('https://ulin-api.herokuapp.com/v1/places');
 
-      const { data } = await response.json();
+//       const { data } = await response.json();
 
-      commit('setPlaces', data);
-    },
+//       commit('setPlaces', data);
+//     },
 
-    async getPlaceById({ commit }, params) {
-      const response = await fetch(`http://ulin-api.herokuapp.com/v1/place/${params}`);
+//     async getPlaceById({ commit }, params) {
+//       const response = await fetch(`http://ulin-api.herokuapp.com/v1/place/${params}`);
 
-      const { data } = await response.json();
-      commit('setPlaceById', data);
-    },
-  },
-};
+//       const { data } = await response.json();
+//       commit('setPlaceById', data);
+//     },
+//   },
+// };
