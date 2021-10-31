f530ccd18ab3dc6ccd4a4a775ce27efa52a54935
saepudinjeh <saepudinjeh@gmail.com>
2021-10-31 23:35:57 +0700

    update position modal form

--- src/components/globalComponents/ModalForm.vue
+++ src/components/globalComponents/ModalForm.vue
@@ -1,10 +1,10 @@
 <template>
   <aside class="absolute inset-0 z-40 w-full h-screen">
     <div class="relative">
-      <div :onclick="closeForm" class="absolute left-0 w-full h-screen bg-black opacity-25">
+      <div :onclick="closeForm" class="absolute left-0 w-full h-screen bg-black opacity-40">
 
       </div>
-      <div class="absolute top-2 left-1/3 z-50 bg-white h-64 w-1/3 rounded-md">
+      <div class="absolute top-12 left-1/3 z-50 bg-white h-64 w-1/3 rounded-md">
         <form class="p-4" method="post">
           <textarea type="text" name="" placeholder="wkwkwkw" class="text-sm w-full focus:outline-none bg-gray-100 p-2 rounded-md"></textarea>
         </form>
