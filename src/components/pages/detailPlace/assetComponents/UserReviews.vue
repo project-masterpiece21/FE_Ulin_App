df71c257fbd45485159a561aab86bb85843d2045
Saepudin <saepudinjeh@gmail.com>
2021-11-23 15:32:51 +0700

    update detail reviews

--- src/components/pages/detailPlace/assetComponents/UserReviews.vue
+++ src/components/pages/detailPlace/assetComponents/UserReviews.vue
@@ -4,9 +4,7 @@
       <div class="flex flex-row w-max gap-x-3">
         <div v-for="review in reviews" :key="review" class="bg-white sm:h-60 sm:w-72 w-52 h-44 rounded-lg shadow-md sm:px-6 sm:py-5 px-4 py-3">
           <div class="flex items-center">
-            <div class="sm:w-12 sm:h-12 w-10 h-10 flex-shrink-0 bg-white shadow rounded-full">
-
-            </div>
+            <img class="sm:w-12 sm:h-12 w-10 h-10 flex-shrink-0 bg-white shadow rounded-full" :src="review.pic" alt=""/>
             <div class="flex flex-col mx-3 leading-tight truncate">
               <h3 class="font-medium sm:text-lg truncate">
                 {{ review.name }}
@@ -35,9 +33,18 @@ export default {
   name: "user-review",
   setup() {
     const reviews = reactive([
-      { name: "Andi sdsadsadsdsadsadsadsadsadsdsadad", message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rem, quisquam magnam, numquam tempora debitis at labore..." },
-      { name: "Cok", message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rem, quisquam magnam, numquam tempora debitis at labore..." },
-      { name: "Ahayyy", message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rem, quisquam magnam, numquam tempora debitis at labore..." }
+      { pic: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+        name: "Andi sdsadsadsdsadsadsadsadsadsdsadad", 
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rem, quisquam magnam, numquam tempora debitis at labore..." 
+      },
+      { pic: 'https://images.unsplash.com/flagged/photo-1573740144655-bbb6e88fb18a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
+        name: "Cok", 
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rem, quisquam magnam, numquam tempora debitis at labore..."
+      },
+      { pic: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+        name: "Ahayyy", 
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rem, quisquam magnam, numquam tempora debitis at labore..."
+      }
     ])
 
     return {
