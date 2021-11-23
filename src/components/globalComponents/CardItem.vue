79da98fe1b39f7433f131c9c6e72c6fea532779e
Saepudin <saepudinjeh@gmail.com>
2021-11-23 15:23:44 +0700

    spa detail route

--- src/components/globalComponents/CardItem.vue
+++ src/components/globalComponents/CardItem.vue
@@ -13,7 +13,7 @@
   >
     <div>
       <figure class="relative overflow-hidden flex-shrink-0">
-        <a :href="`place/${id}`">
+        <router-link :to="{ name: 'place-detail', params: { id: id }}">
           <div
             class="
               md:aspect-w-5 md:aspect-h-3
@@ -27,7 +27,7 @@
               class="object-cover"
             />
           </div>
-        </a>
+        </router-link>
 
         <div
           class="
